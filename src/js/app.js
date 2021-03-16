770afd36b512a5a4a46634cf0fabaed45378e031
Dima <webbootcamp2020@gmail.com>
2021-03-16 22:54:24 +0200

    Built contact page

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 import '../sass/style.scss';
 
 import { toggleClass } from './utils';
-import './vendors/swiperInit.js';
 
+import './vendors/swiperInit.js';
 import Header from './components/Header';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const header = new Header('main-header');
@@ -16,4 +19,21 @@ regionDropdown.addEventListener('click', () => {
 });
 regionList.addEventListener('click', (e) => {
 	console.log(e);
+});
+
+window.addEventListener('click', (e) => {
+	if (!e.target.closest('.dropdown--region')) {
+		if (regionDropdown.classList.contains('open')) {
+			toggleClass(regionDropdown, 'open');
+		}
+	}
+})
+
+
+// Init AOS
+AOS.init({
+	duration: 1200,
+	easing: 'easeInOutQuad',
+	delay: 50,
+	offset: 50,
 });
